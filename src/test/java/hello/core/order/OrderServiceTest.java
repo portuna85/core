f4e3cb4f8519ae782c47f1197863b10459c12bdf
portuna85 <portuna85@gmail.com>
2022-08-28 09:08:36 +0900

    Spring Basic Study

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,35 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    public void 주문생성() throws Exception {
+        // given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        // when
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        // then
+        assertThat(order.getItemName()).isEqualTo("itemA");
+
+    }
+
+}
