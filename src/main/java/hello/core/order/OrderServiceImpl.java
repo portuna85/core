4fad8e02756e75814288086e969f35dd6942ad3d
portuna85 <portuna85@gmail.com>
2022-08-30 14:48:06 +0900

    Spring Basic UPDATE

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,17 +3,21 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        log.info("memberRepository = {}", memberRepository);
+        log.info("discountPolicy = {}", discountPolicy);
+        
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
