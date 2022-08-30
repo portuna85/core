9c5e156a74b1790932ef0a5883171caba6d8dbf7
portuna85 <portuna85@gmail.com>
2022-08-30 17:59:21 +0900

    Spring Basic UPDATE

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 @Slf4j
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
 
     private String url;
 
@@ -32,14 +32,12 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         log.info("close = {}", url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init(){
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         log.info("NetworkClient.destory");
         disconnect();
     }
